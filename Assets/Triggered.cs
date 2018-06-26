d737b7fdf4b4ae2bac8a903a19e310cb54074d40
Cheezegami <superronald@live.nl>
2018-06-26 14:45:27 +0200

    changes

---
+++ Assets/Triggered.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Triggered : MonoBehaviour {
+    public bool hasSwitch;
+    public GameObject text;
+    public Animator anim;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player") {
+            text.SetActive(true);
+            if(hasSwitch) anim.SetTrigger("GoSad");
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if(other.tag == "Player") {
+            text.SetActive(false);
+            if(hasSwitch) anim.SetTrigger("GoIdle");
+        }
+    }
+}
