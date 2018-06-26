ec688711324fa831f4de58fc0127fa85cc890db7
Cheezegami <superronald@live.nl>
2018-06-26 14:29:39 +0200

    All the changes

---
+++ Assets/ChooseRandomBot.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseRandomBot : MonoBehaviour {
+    public GameObject[] robotz;
+	// Use this for initialization
+	void Start () {
+        robotz[Random.Range(0, robotz.Length)].SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
